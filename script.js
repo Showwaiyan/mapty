129f27418b7e7316ee4a632f5710326f1a875a48
Show Wai Yan <showwaiyan555@gmail.com>
2025-01-07 12:16:55 +0800

    Feature: implemented map api from leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) navigator.geolocation.getCurrentPosition(function(pos) {
+    const {latitude, longitude} = pos.coords;
+    const zoomLevel = 16;
+    const map = L.map('map').setView([latitude, longitude], zoomLevel);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+}, function() {
+    alert("Can't fetch the location!");
+})
